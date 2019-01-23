4805950d55d6374921e58b8d255b83e369ec2322
fmaylinch <ferranmaylinch@gmail.com>
2019-01-23 11:09:39 -0500

    Escribir un test para cada causistica

--- src/main/java/com/platzi/javatests/util/StringUtil.java
+++ src/main/java/com/platzi/javatests/util/StringUtil.java
@@ -4,6 +4,10 @@ public class StringUtil {
 
     public static String repeat(String str, int times) {
 
+        if (times < 0) {
+            throw new IllegalArgumentException("negative times not allowed");
+        }
+
         String result = "";
 
         for (int i = 0; i < times; i++) {
