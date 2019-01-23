09a32d80d31526a302aabd1e43954f7c3247a504
fmaylinch <ferranmaylinch@gmail.com>
2019-01-23 12:38:16 -0500

    Test con mockito simulando una pasarela de pago

---
+++ src/test/java/com/platzi/javatests/payments/PaymentProcessorTest.java
@@ -0,0 +1,33 @@
+package com.platzi.javatests.payments;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class PaymentProcessorTest {
+
+    @Test
+    public void payment_is_correct() {
+
+        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+        Mockito.when(paymentGateway.requestPayment(Mockito.any()))
+                .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
+
+        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+
+        assertTrue(paymentProcessor.makePayment(1000));
+    }
+
+    @Test
+    public void payment_is_wrong() {
+
+        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+        Mockito.when(paymentGateway.requestPayment(Mockito.any()))
+                .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
+
+        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+
+        assertFalse(paymentProcessor.makePayment(1000));
+    }
+}
