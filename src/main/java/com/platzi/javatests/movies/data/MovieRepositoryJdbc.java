1908e6ce34bb97a69e95704642e2223eab3c1d54
fmaylinch <ferranmaylinch@gmail.com>
2019-01-24 11:49:12 -0500

    App de películas - buscar una película

--- src/main/java/com/platzi/javatests/movies/data/MovieRepositoryJdbc.java
+++ src/main/java/com/platzi/javatests/movies/data/MovieRepositoryJdbc.java
@@ -17,7 +17,10 @@ public class MovieRepositoryJdbc implements MovieRepository {
 
     @Override
     public Movie findById(long id) {
-        return null;
+
+        Object[] args = { id };
+
+        return jdbcTemplate.queryForObject("select * from movies where id = ?", args, movieMapper);
     }
 
     @Override
