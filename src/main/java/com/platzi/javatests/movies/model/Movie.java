5a16ae8aae01a3948840b39a8a6672cdd3eb1ec5
fmaylinch <ferranmaylinch@gmail.com>
2019-01-24 11:22:38 -0500

    App de películas - repositorio con JDBC

--- src/main/java/com/platzi/javatests/movies/model/Movie.java
+++ src/main/java/com/platzi/javatests/movies/model/Movie.java
@@ -1,5 +1,7 @@
 package com.platzi.javatests.movies.model;
 
+import java.util.Objects;
+
 public class Movie {
 
     private Integer id;
@@ -32,5 +34,21 @@ public class Movie {
 
     public Genre getGenre() {
         return genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return minutes == movie.minutes &&
+                Objects.equals(id, movie.id) &&
+                Objects.equals(name, movie.name) &&
+                genre == movie.genre;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, minutes, genre);
     }
 }
